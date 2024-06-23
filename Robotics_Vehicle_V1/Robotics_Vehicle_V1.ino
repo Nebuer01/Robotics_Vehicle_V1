3d911ac6c6c078bd5cf534706241ee5b7b469432
Nebuer <146623278+Nebuer01@users.noreply.github.com>
2024-06-24 09:29:50 +1200

    Added Digital Pin Defines

--- Robotics_Vehicle_V1/Robotics_Vehicle_V1.ino
+++ Robotics_Vehicle_V1/Robotics_Vehicle_V1.ino
@@ -4,6 +4,24 @@
     Author:     MIMAS\reube
 */
 
+#pragma region PinDefine
+
+#define RX 0;
+#define TX 1;
+//#define null 2;
+#define servo 3;
+#define motor1 4;
+#define redLED 5;
+//#define null 6;
+#define motor2 7;
+#define motor3 8;
+#define motor4 9;
+#define IR 10;
+#define blueLED 11;
+#define ultraTrig 12;
+#define ultraEcho 13;
+
+#pragma endregion
 
 void setup()
 {
